259dd24ce6bf81c90144776a665cf00ce0cc5d17
henrynguci <henrynguci@gmail.com>
2024-10-25 18:13:46 +0700

    import model and test crud

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,17 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import colors from 'colors';
+
+// models
+import documentSchema from './models/document.js';
+import feedbackSchema from './models/feedback.js';
+import notificationSchema from './models/notification.js';
+import pageOrderSchema from './models/pageOrder.js';
+import printerSchema from './models/printer.js';
+import printOrderSchema from './models/printOrder.js';
+import spsoSchema from './models/spso.js';
+import userSchema from './models/user.js';
 
 dotenv.config();
 
@@ -24,12 +35,47 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// CRUD
+// Create
+app.post('/api/documents', async (req, res) => {
+    try {
+        const document = await documentSchema.create(req.body);
+        res.status(201).json({ document });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.post('/api/feedbacks', async (req, res) => {
+    try {
+        const feedback = await feedbackSchema.create(req.body);
+        res.status(201).json({ feedback });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.post('/api/notifications', async (req, res) => {
+    try {
+        const notification = await notificationSchema.create(req.body);
+        res.status(201).json({ notification });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.post('/api/pageOrders', async (req, res) => {
+    try {
+        const pageOrder = await pageOrderSchema.create(req.body);
+        res.status(201).json({ pageOrder });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .bold,
-    );
+    console.log(`Server running on port ${PORT}`.yellow.bold);
 });
 
 app.use((req, res) => {
