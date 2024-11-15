4a4d4a35984b11c5ba1896269e0557acea6a8d8d
duykhangtong <khang.tong1124@hcmut.edu.vn>
2024-11-15 08:14:03 +0700

    refactor: refactor feedback entity

--- backend/src/controllers/feedback.controller.js
+++ backend/src/controllers/feedback.controller.js
@@ -1,50 +1,44 @@
 
-import pool from "../config/db.js";
-import * as feedbackQueries from "../services/feedback.service.js";
+import * as feedbackService from "../services/feedback.service.js";
 
-export const getAllFeedback = (req, res) => {
-    pool.query(feedbackQueries.getAllFeedbacks, (error, results) => {
-        if (results.rows.length === 0) {
-            return res.send("There is no feedback in the db!!");
-        }
-
-        res.status(200).json(results.rows);
-    });
+export const addFeedback = async (req, res) => {
+    try {
+        const dataForm = req.body;
+        await feedbackService.addFeedback(dataForm);
+        return res.status(201).json();
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    }
 };
 
-export const getFeedbackById = (req, res) => {
-    const id = parseInt(req.params.id);
-    pool.query(feedbackQueries.getFeedbackById, [id], (error, results) => {
-        if (error) throw error;
-
-        if (results.rows.length === 0) {
-            return res.send("This feedbak doesn't exist in the db!!!");
-        }
-
-        res.status(200).json(results.rows);
-    })
+export const getAllFeedback = async (req, res) => {
+    try {
+        const feedbacks = await feedbackService.getAllFeedback();
+        return res.status(200).json(feedbacks);
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    }
 };
 
-export const addFeedback = (req, res) => {
-    const { message, rating } = req.body;
-    pool.query(feedbackQueries.addFeedback, [message, rating], (error, results) => {
-        if (error) throw error;
-        res.status(201).send('Add feedback successfully!!');
-    })
+export const getFeedbackById = async (req, res) => {
+    try {
+        const feedback = await feedbackService.getAllFeedback(req.params.id);
+        return res.status(200).json(feedback);
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    }
 };
 
-export const deleteFeedback = (req, res) => {
-    const id = parseInt(req.params.id);
-    pool.query(feedbackQueries.getFeedbackById, [id], (error, results) => {
 
-        if (results.rows.length === 0) {
-            return res.send("This feedbak doesn't exist in the db!!!");
-        }
-
-        pool.query(feedbackQueries.deleteFeedback, [id], (error, results) => {
-            if (error) throw error;
-
-            res.status(200).send("Deleted feedback successfully!!");
-        })
-    })
-};
+export const deleteFeedback = async (req, res) => {
+    try {
+        await feedbackService.deleteFeedback(req.params.id);
+        return res.status(200);
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    }
+}
